cfbe1fef426258bc288f6f33daf4b7eb5a9c71f0
runtime-error786 <mustafa782a@gmail.com>
2024-02-17 22:50:43 +0500

    home section added

--- src/app/Dash/Serv.js
+++ src/app/Dash/Serv.js
@@ -5,45 +5,42 @@ import "./g.css";
 const Kani1 = Anton({
     weight: '400',
     subsets: ['latin'],
-});
-
-export default function Exp({ fw }) {
+})
+export default function Exp({fw}) {
     return (
-        <div ref={fw} className="container-fluid d-flex justify-content-center align-items-center">
-            <div className="row">
-                <div className="col-lg-12 text-center mt-5">
-                    <h1 className={`k1 ${Kani1.className}`}> Services</h1>
-                </div>
-                <div className='row align-items-center mt-5 ps-3'>
-                    <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
-                        <div className={`card text-center ${window.innerWidth <= 576 ? 'w-100' : 'w-75'} ms-5`}>
+        <div ref={fw} className="container-fluid d-flex justify-content-center align-items-center mt-5 pt-5">
+            <div className="row ">
+                <h1 className={`text-center ${Kani1.className} k1 mt-2`}>Services</h1>
+                <div className='row align-items-center justify-content-center mt-5 negative-gutter'> {/* Centering the row */}
+                    <div className="text-center justify-content-center align-align-items-center d-flex col-lg-3 col-md-6 col-sm-12 mb-3">
+                        <div className="card text-center ms-md-5">
                             <div className="card-body">
-                                <h5 className="card-title">UI/UX Design</h5>
+                                <h5 className={`card-title ${Kani1.className}`}>UI/UX Design</h5>
                                 <p className="card-text">Elevate your digital presence with our expertly crafted UI designs, tailored to captivate your audience and drive engagement.
                                     Transform your vision into reality with our interactive prototyping services, offering a glimpse into the intuitive user experiences we create</p>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
-                        <div className={`card text-center ${window.innerWidth <= 576 ? 'w-100' : 'w-75'} ms-5`}>
+                    <div className="text-center justify-content-center align-align-items-center d-flex col-lg-3 col-md-6 col-sm-12 mb-3">
+                        <div className="card text-center ms-md-5" >
                             <div className="card-body">
-                                <h5 className="card-title">Website Design</h5>
+                                <h5 className={`card-title ${Kani1.className}`}>Website Design</h5>
                                 <p className="card-text">Empower your web presence with our frontend development expertise, harnessing the capabilities of React.js for stunning user interfaces. Rely on our backend development prowess to build robust APIs with Node.js and Express.js, ensuring seamless data management and integration.</p>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
-                        <div className={`card text-center ${window.innerWidth <= 576 ? 'w-100' : 'w-75'} ms-5`}>
+                    <div className="text-center justify-content-center align-align-items-center d-flex col-lg-3 col-md-6 col-sm-12 mb-3">
+                        <div className="card text-center  ms-md-5">
                             <div className="card-body">
-                                <h5 className="card-title">Application Design</h5>
+                                <h5 className={`card-title ${Kani1.className}`}>Application Design</h5>
                                 <p className="card-text">Accelerate your mobile app development journey with React Native, delivering cross-platform experiences without compromising on performance. Optimize your React Native apps for speed and efficiency, ensuring smooth animations and responsive interactions on every device.</p>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4 col-md-6 col-sm-12 offset-lg-4 mb-3">
-                        <div className={`card text-center ${window.innerWidth <= 576 ? 'w-100' : 'w-75'} ms-5`}>
+                    <div className="text-center justify-content-center align-align-items-center d-flex col-lg-3 col-md-6 col-sm-12 mb-3">
+                        <div className="card text-center  ms-md-5">
                             <div className="card-body">
-                                <h5 className="card-title">Windows Form(GUI)</h5>
+                                <h5 className={`card-title ${Kani1.className}`}>Windows Form(GUI)</h5>
                                 <p className="card-text">I offer expert Graphical User Interface (GUI) services, crafting intuitive designs that elevate user experience and streamline interaction with software applications. From wireframing to final implementation, I ensure seamless navigation and visually appealing interfaces across desktop platforms.</p>
                             </div>
                         </div>
