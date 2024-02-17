2bee398e602f39d9c5e7e77cb74ccbe27ca66bc9
runtime-error786 <mustafa782a@gmail.com>
2024-02-17 10:50:13 +0500

    home section added

--- src/app/Dash/Home.js
+++ src/app/Dash/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
         <div className="container-fluid d-flex justify-content-center align-items-center" style={{ minHeight: "100vh" }}>
             <div className="row m-5 p-5">
                 <div className="col-lg-7  text-center mt-lg-5 mt-md-2 pt-lg-1 mb-md-5">
-                    <h4 className="as1">Hello, It's Me</h4>
+                    <h4 className="as1">Hello, It&apos;s Me</h4>
                     <h1 className="as2">Mustafa Rizwan</h1>
                     <h4 >And I am <span className="as3">Full Stack Developer</span></h4>
                 </div>
--- src/app/Dash/Img.js
+++ src/app/Dash/Img.js
@@ -1,10 +1,11 @@
-import Image from "next/image";
-import Pic from "../../../public/pic.jpg";
 
-export default function Img() {
-    return (
+import Image from "next/image"
+import Pic from "../../../public/pic.jpg"
+export default function Img()
+{
+    return(
         <>
-            <Image src={Pic} className={"rounded-5 " + (window.innerWidth > 425 ? 'w-75' : 'w-100')} alt="Profile picture" />
+            <Image src={Pic} className="w-75 h-75 rounded-5 g1 " alt="image is here"></Image>
         </>
     )
 }
