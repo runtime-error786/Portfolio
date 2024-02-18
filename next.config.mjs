b30600da4b1ea7b82b74c42b9e080f5362031da3
runtime-error786 <mustafa782a@gmail.com>
2024-02-18 16:34:25 +0500

    s

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,12 @@
 
 const nextConfig = {
     images: { unoptimized: true },
+    // Configure output to export
+    output: 'export',
     exportPathMap: function () {
         return {
             '/': { page: '/' },
-            '/Dash':{page: '/Dash'},
+            '/Dash': { page: '/Dash' },
             // Add other pages if necessary
         };
     },
