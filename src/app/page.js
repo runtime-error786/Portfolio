524f8a9cfc5846d40e8c1fc98fb28e59cb28dad7
runtime-error786 <mustafa782a@gmail.com>
2024-02-18 12:43:43 +0500

    adding directly routing to dash

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { useEffect } from 'react';
 
 export default function Home() {
-  return (
-    <>
-      
-    </>
-  );
+  const router = useRouter();
+
+  // Redirect to the Dash page when the component mounts
+ useEffect(() => {
+    router.replace('/Dash');
+  }, []);
+
+  // This component doesn't render anything since it redirects immediately
+  return null;
 }
